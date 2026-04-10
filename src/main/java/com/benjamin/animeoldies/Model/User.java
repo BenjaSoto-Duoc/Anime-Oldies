da25af7fd77815f1a1979d108f6328f0b0d1349c
Benjamin Soto <benja.sotog@duocuc.cl>
2026-04-10 14:04:25 -0400

    Added Jakarta Validation

---
+++ src/main/java/com/benjamin/animeoldies/Model/User.java
@@ -0,0 +1,25 @@
+package com.benjamin.animeoldies.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    private String nickname;
+    private String email;
+    private String passwd;
+}
